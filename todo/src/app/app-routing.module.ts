7c1b1ae0a81e8db1bf9d0fdebde495b7746ae7e5
DimitarMilushev <deadlycookye@gmail.com>
2021-09-27 11:05:01 +0300

    fixed component path

--- todo/src/app/app-routing.module.ts
+++ todo/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BoardComponent } from './core/components/board/board.component';
+import { BoardComponent } from './core/components/board-components/board/board.component';
 
 const routes: Routes = [
   {path: 'board', component: BoardComponent}
